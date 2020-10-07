3a2dae9b7fcfc718bc62878d0931b40f736b4a34
Pierce Corcoran <code@thecodewarrior.dev>
2020-10-07 12:43:47 -0700

    Adjust output formatting

--- src/main.cpp
+++ src/main.cpp
@@ -62,6 +62,7 @@ int run_day(int day, aoc::DayResult(*function)(std::ostream &)) {
     aoc::DayResult day_result = function(out);
     out.flush();
     if (day_result.return_code == 0) {
+        bool was_success = true;
         for (const auto &result : day_result.results) {
             size_t width = 0;
 
@@ -78,7 +79,10 @@ int run_day(int day, aoc::DayResult(*function)(std::ostream &)) {
             text << ANSI_FG_GREEN << ": ";
             width += 2;
 
+            std::string line_color;
             if (result.value != result.correct_value) {
+                line_color = ANSI_FG_RED;
+                was_success = false;
                 if (result.is_trivia) {
                     text << ANSI_FG_RED << result.value << ANSI_FG_GREEN << " (" << ANSI_FG_LIGHT_GREEN
                          << result.correct_value << ANSI_FG_GREEN << ")";
@@ -89,6 +93,7 @@ int run_day(int day, aoc::DayResult(*function)(std::ostream &)) {
                 }
                 width += result.value.size() + result.correct_value.size() + 3;
             } else {
+                line_color = ANSI_FG_GREEN;
                 if (result.is_trivia) {
                     text << ANSI_FG_LIGHT_GREEN << result.value;
                 } else {
@@ -97,7 +102,7 @@ int run_day(int day, aoc::DayResult(*function)(std::ostream &)) {
                 width += result.value.size();
             }
 
-            text << ANSI_FG_GREEN << " ";
+            text << line_color << " ";
             width++;
 
             std::string pad, lcap, rcap;
@@ -115,19 +120,29 @@ int run_day(int day, aoc::DayResult(*function)(std::ostream &)) {
             if (!result.part_name.empty()) {
                 std::string part;
                 if (result.is_trivia) {
-                    part = std::string(" ") + ANSI_FG_LIGHT_GREEN + result.part_name + ANSI_FG_GREEN + " ";
+                    part = std::string(" ") + ANSI_FG_LIGHT_GREEN + result.part_name + line_color + " ";
                 } else {
-                    part = std::string(" ") + ANSI_FG_LIGHT_YELLOW + result.part_name + ANSI_FG_GREEN + " ";
+                    part = std::string(" ") + ANSI_FG_LIGHT_YELLOW + result.part_name + line_color + " ";
                 }
                 size_t l = pad.size();
                 centered.replace(l, (result.part_name.size() + 2) * l, part);
             }
 
-            std::cout << ANSI_FG_GREEN << lcap << centered << rcap << ANSI_FG_DEFAULT
+            std::cout << ANSI_FG_GREEN << lcap << line_color << centered << ANSI_FG_GREEN << rcap << ANSI_FG_DEFAULT
                       << std::endl;
         }
 
-        std::cout << ANSI_FG_GREEN << "╚" << center("═", pad_width, std::string(" Success "), 9) << "╝"
+        std::stringstream result_message;
+        size_t result_length = 0;
+
+        if(was_success) {
+            result_message << ANSI_FG_LIGHT_YELLOW << ANSI_BOLD_ON << " Success " << ANSI_BOLD_OFF << ANSI_FG_GREEN;
+            result_length += 9;
+        } else {
+            result_message << ANSI_FG_RED << ANSI_BOLD_ON << " Failure " << ANSI_BOLD_OFF << ANSI_FG_GREEN;
+            result_length += 9;
+        }
+        std::cout << ANSI_FG_GREEN << "╚" << center("═", pad_width, result_message.str(), result_length) << "╝"
                   << ANSI_FG_DEFAULT << std::endl;
     } else {
         size_t width = 0;
